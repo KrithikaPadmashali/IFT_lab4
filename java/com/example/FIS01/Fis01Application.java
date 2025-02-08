c62a0c62b4913bab02844845f32705e34dd3e5b0
Krithika <165032178+KrithikaPadmashali@users.noreply.github.com>
2025-02-08 16:09:44 +0530

    Add files via upload

---
+++ java/com/example/FIS01/Fis01Application.java
@@ -0,0 +1,13 @@
+package com.example.FIS01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fis01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fis01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FIS01/Fis01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FIS01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fis01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
