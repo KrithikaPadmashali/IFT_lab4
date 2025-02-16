0e7c4822763c5ffafdb2f3e2d06b902a0850ec34
Krithika <165032178+KrithikaPadmashali@users.noreply.github.com>
2025-02-16 23:23:11 +0530

    Add files via upload

---
+++ demo_question/src/main/java/com/example/demo_question/DemoQuestionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoQuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoQuestionApplication.class, args);
+	}
+
+}
