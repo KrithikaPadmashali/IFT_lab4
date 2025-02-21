adb7269fa54a50c8380d9096df547a349a352244
Krithika <165032178+KrithikaPadmashali@users.noreply.github.com>
2025-02-21 17:13:21 +0530

    Delete question1_4 directory

--- question1_4/src/main/java/com/example/question1_4/repository/CUST_CIRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.question1_4.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.question1_4.entity.CUST_CI;
-
-public interface CUST_CIRepository extends JpaRepository<CUST_CI, Long> {
-}
